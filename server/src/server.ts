e52c7d9e84e6d2a465afeaf4e79b5d469f233c2c
João Victor <joaovictorfernandescastro@gmail.com>
2026-04-26 14:47:45 -0300

    refactor(server): simplify server bootstrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
 import { fastify } from 'fastify'
-import { producer } from './kafka'
-import { startConsumer } from './consumer'
 import {
   serializerCompiler,
   validatorCompiler,
@@ -37,31 +35,13 @@ app.register(ScalarApiReference, {
   routePrefix: '/docs',
 })
 
-app.post('/test-kafka', async (request, reply) => {
-  await producer.send({
-    topic: 'hub44.test',
-    messages: [
-      { value: 'Mensagem enviada pelo backend ' }
-    ],
+app
+  .listen({ port: 3333, host: '0.0.0.0' })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333 !')
+    console.log('Docs available at http://localhost:3333/docs')
   })
-
-  return {
-    success: true,
-    message: 'Mensagem enviada para o Kafka!',
-  }
-})
-
-async function start() {
-  await producer.connect()
-
-  await startConsumer() // 👈 aqui
-
-  await app.listen({ port: 3333, host: '0.0.0.0' })
-
-  console.log('HTTP server running on http://localhost:3333 !')
-  console.log('Docs available at http://localhost:3333/docs')
-  console.log('Kafka producer connected on localhost:9092')
-  console.log('Kafka consumer running and listening...')
-}
-
-start()
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
