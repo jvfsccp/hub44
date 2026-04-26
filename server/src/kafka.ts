51e9f60e8334dbfe6fe552c03b0b24f5ca0f0cee
João Victor <joaovictorfernandescastro@gmail.com>
2026-04-26 14:47:45 -0300

    refactor(server): remove kafka module from src root

--- server/src/kafka.ts
+++
@@ -1,9 +0,0 @@
-import { Kafka } from 'kafkajs'
-
-export const kafka = new Kafka({
-  clientId: 'hub44-server',
-  brokers: ['localhost:9092'],
-})
-
-export const producer = kafka.producer()
-export const consumer = kafka.consumer({ groupId: 'hub44-group' })
