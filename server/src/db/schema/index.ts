a8edf6670e7bb77ca4f7f6aacb02885801dc81c8
João Victor <joaovictorfernandescastro@gmail.com>
2026-05-15 10:41:23 -0300

    feat: export order schemas

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -1,5 +1,14 @@
 export { default as addresses } from './addresses'
 export { default as categories } from './categories'
+export { default as notifications, notificationTypes } from './notifications'
+export { default as orderEvents } from './order-events'
+export { default as orderItems } from './order-items'
+export {
+  default as orders,
+  orderStatuses,
+  paymentMethods,
+  paymentStatuses,
+} from './orders'
 export { default as products, productStatuses } from './products'
 export { default as stores, storeStatuses } from './stores'
 export { default as users, userRoles } from './users'
